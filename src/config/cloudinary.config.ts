967ac6f95f9ea174e444a4dd302bc4d9c497a6f4
Ebrahim Elghrbawy <HIMA132004@GMAIL.COM>
2026-02-12 11:40:37 +0200

    refactor(configration): update cloudinary and db configration use RegisterAs to set namespace for config collection

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -1,7 +1,11 @@
+import { registerAs } from '@nestjs/config';
 import { ConfigOptions } from 'cloudinary';
 
-export default (): ConfigOptions => ({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'your-cloud-name',
-  api_key: process.env.CLOUDINARY_API_KEY || 'your-api-key',
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+export default registerAs(
+  'cloudinary',
+  (): ConfigOptions => ({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'your-cloud-name',
+    api_key: process.env.CLOUDINARY_API_KEY || 'your-api-key',
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  }),
+);
--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,13 +1,17 @@
+import { registerAs } from '@nestjs/config';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions.js';
 
-export default (): PostgresConnectionOptions => ({
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: +process.env.DB_PORT! || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || '123',
-  database: process.env.DB_DATABASE || 'nestjs',
-  entities: [__dirname + '../../**/*.entity{.ts,.js}'],
-  synchronize: true,
-  logging: true,
-});
+export default registerAs(
+  'database',
+  (): PostgresConnectionOptions => ({
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: +process.env.DB_PORT! || 5432,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || '123',
+    database: process.env.DB_DATABASE || 'nestjs',
+    entities: [__dirname + '../../**/*.entity{.ts,.js}'],
+    synchronize: true,
+    logging: true,
+  }),
+);
