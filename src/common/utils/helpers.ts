c49991a8de7973c02f3a55cb2799cb98b9c94852
Ebrahim Elghrbawy <HIMA132004@GMAIL.COM>
2026-03-03 01:20:16 +0200

    feat(common): created paseTimeToCronExpression function

--- src/common/utils/helpers.ts
+++ src/common/utils/helpers.ts
@@ -6,3 +6,30 @@ export const generateSlug = (name: string): string => {
     .replace(/\s+/g, '-')
     .replace(/-+/g, '-');
 };
+
+export const parseTimeToCronExpression = (time: string): string => {
+  const match = time.match(/^(\d+)([smhd])$/);
+  if (!match) {
+    throw new Error(
+      `Invalid expiration time format: ${time}. Expected format is a number followed by 's', 'm', 'h', or 'd'.`,
+    );
+  }
+
+  const value = parseInt(match[1], 10);
+  const unit = match[2];
+
+  switch (unit) {
+    case 's':
+      return `*/${value} * * * * *`;
+    case 'm':
+      return `0 */${value} * * * *`;
+    case 'h':
+      return `0 0 */${value} * * *`;
+    case 'd':
+      return `0 0 0 */${value} * *`;
+    default:
+      throw new Error(
+        `Invalid time unit: ${unit}. Expected 's', 'm', 'h', or 'd'.`,
+      );
+  }
+};
