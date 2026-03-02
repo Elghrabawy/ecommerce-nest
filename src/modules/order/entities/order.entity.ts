06d435bc87736da14535c60c406a6be3d720542e
Ebrahim Elghrbawy <HIMA132004@GMAIL.COM>
2026-03-03 01:19:45 +0200

    feat(order): added index orders (status and created at) to optimize qqueries filtering by status and created at (for expiration checks)

--- src/modules/order/entities/order.entity.ts
+++ src/modules/order/entities/order.entity.ts
@@ -1,12 +1,14 @@
 import { User } from 'src/modules/user/entities/user.entity';
 import { BaseEntity } from 'src/common/entities';
 import { OrderStatus } from 'src/common/utils/enums';
-import { Entity, Column, ManyToOne, OneToMany, OneToOne } from 'typeorm';
+import { Entity, Column, ManyToOne, OneToMany, OneToOne, Index } from 'typeorm';
 import { OrderItem } from './order-item.entity';
 import { Payment } from 'src/modules/payment/entities/payment.entity';
 import { Address } from 'src/modules/address/entities/address.entity';
 
 @Entity('orders')
+// index to optimize queries filtering by status and created_at (for expiration checks)
+@Index('IDX_orders_status_created_at', ['status', 'created_at'])
 export class Order extends BaseEntity {
   @ManyToOne(() => User, (user) => user.orders)
   user: User;
