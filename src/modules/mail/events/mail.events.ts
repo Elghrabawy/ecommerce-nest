0a84d34beb5c942b7b3377c42816d1e435ca78ca
Ebrahim Elghrbawy <HIMA132004@GMAIL.COM>
2026-03-02 21:01:35 +0200

    feat(email): add some mail events and refactor payment success event - add amout property to payment success event - created payment failed event - created order expired event - created oder cancelled event - created refunded event

--- src/modules/mail/events/mail.events.ts
+++ src/modules/mail/events/mail.events.ts
@@ -17,5 +17,37 @@ export class PaymentSuccessEvent {
   constructor(
     public readonly email: string,
     public readonly orderId: number,
+    public readonly amount: number,
+  ) {}
+}
+
+export class PaymentFailedEvent {
+  constructor(
+    public readonly email: string,
+    public readonly orderId: number,
+    public readonly reason: string,
+  ) {}
+}
+
+export class OrderExpiredEvent {
+  constructor(
+    public readonly email: string,
+    public readonly orderId: number,
+  ) {}
+}
+
+export class OrderCancelledEvent {
+  constructor(
+    public readonly email: string,
+    public readonly orderId: number,
+    public readonly reason: string,
+  ) {}
+}
+
+export class RefundedEvent {
+  constructor(
+    public readonly email: string,
+    public readonly orderId: number,
+    public readonly amount: number,
   ) {}
 }
