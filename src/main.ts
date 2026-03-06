2aebac5d7d94054b32e90f8abeee30c989d80f6a
Ebrahim Elghrbawy <HIMA132004@GMAIL.COM>
2026-03-07 01:05:54 +0200

    start configure vercel

--- src/main.ts
+++ src/main.ts
@@ -40,4 +40,7 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Error starting application:', err);
+  process.exit(1);
+});
